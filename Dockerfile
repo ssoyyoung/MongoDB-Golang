48e5a04dbe6c373fc778b49a38faa071669e45ba
ssoyyoung <ssoyyoung.p@gmail.com>
2020-05-11 00:13:04 +0900

    cors error solution 1

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM golang:1.14
 
 WORKDIR /go/src/github.com/ssoyyoung.p/MongoDB-Golang
-COPY . /go/src/github.com/ssoyyoung.p/MongoDB-Golang
+#COPY . /go/src/github.com/ssoyyoung.p/MongoDB-Golang
 
 #RUN go get go.mongodb.org/mongo-driver
 RUN go get -v -u go.mongodb.org/mongo-driver/mongo
--- main.go
+++ main.go
@@ -25,12 +25,45 @@ func getList(c echo.Context) error {
 
 func main() {
 	e := echo.New()
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"https://localhost", "https://49.247.134.77"},
-		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
-	}))
+
+	e.Use(CORSMiddlewareWrapper)
+	//e.Use(middleware.CORS())
+	//e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+	//	AllowOrigins: []string{"*"},
+	//	AllowHeaders: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+	//}))
+
 	e.GET("/", goPage)
 	e.GET("/saveData", saveHandler)
-	e.GET("/getList", getList)
+	e.GET("/getList", indexHandler)
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+    (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	setupResponse(&w, req)
+	if (*req).Method == "OPTIONS" {
+		return
+	}
+	res := mongodb.ListData()
+
+	return c.String(http.StatusOK, res)
+}
+
+func CORSMiddlewareWrapper(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(ctx echo.Context) error {
+		req := ctx.Request()
+		dynamicCORSConfig := middleware.CORSConfig{
+			AllowOrigins: []string{req.Header.Get("Origin")},
+			AllowHeaders: []string{"Accept", "Cache-Control", "Content-Type", "X-Requested-With"},
+		}
+		CORSMiddleware := middleware.CORSWithConfig(dynamicCORSConfig)
+		CORSHandler := CORSMiddleware(next)
+		return CORSHandler(ctx)
+	}
 }
