d12f1a17da3be3c8709054075fe9e1b792fa899c
ssoyyoung <ssoyyoung.p@gmail.com>
2020-05-11 00:16:32 +0900

    cors error solution1-1

--- main.go
+++ main.go
@@ -35,26 +35,11 @@ func main() {
 
 	e.GET("/", goPage)
 	e.GET("/saveData", saveHandler)
-	e.GET("/getList", indexHandler)
+	e.GET("/getList", getList)
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
-func setupResponse(w *http.ResponseWriter, req *http.Request) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-    (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-}
-
-func indexHandler(w http.ResponseWriter, req *http.Request) {
-	setupResponse(&w, req)
-	if (*req).Method == "OPTIONS" {
-		return
-	}
-	res := mongodb.ListData()
-
-	return c.String(http.StatusOK, res)
-}
-
+// CORSMiddlewareWrapper func
 func CORSMiddlewareWrapper(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(ctx echo.Context) error {
 		req := ctx.Request()
