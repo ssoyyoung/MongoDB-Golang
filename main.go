1fd3f6bd5d73fb891241d631ba633a6f80e3e073
ssoyyoung <ssoyyoung.p@gmail.com>
2020-05-11 23:34:24 +0900

    clean code

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"net/http"
-	
+
 	"github.com/labstack/echo"
 	mongodb "github.com/ssoyyoung.p/MongoDB-Golang/mongo"
 )
@@ -17,7 +17,7 @@ func saveHandler(c echo.Context) error {
 }
 
 func getList(c echo.Context) error {
-	c.Response().Header().Set("Access-Control-Allow-Origin","*")
+	c.Response().Header().Set("Access-Control-Allow-Origin", "*")
 	res := mongodb.ListData()
 
 	return c.String(http.StatusOK, res)
@@ -25,30 +25,10 @@ func getList(c echo.Context) error {
 
 func main() {
 	e := echo.New()
-	
-	//e.Use(CORSMiddlewareWrapper)
-	//e.Use(middleware.CORS())
-	//e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-	//	AllowOrigins: []string{"*"},
-	//	AllowHeaders: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
-	//}))
 
 	e.GET("/", goPage)
 	e.GET("/saveData", saveHandler)
 	e.GET("/getList", getList)
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
-
-// CORSMiddlewareWrapper func
-/* func CORSMiddlewareWrapper(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(ctx echo.Context) error {
-		req := ctx.Request()
-		dynamicCORSConfig := middleware.CORSConfig{
-			AllowOrigins: []string{req.Header.Get("Origin")},
-			AllowHeaders: []string{"Accept", "Cache-Control", "Content-Type", "X-Requested-With"},
-		}
-		CORSMiddleware := middleware.CORSWithConfig(dynamicCORSConfig)
-		CORSHandler := CORSMiddleware(next)
-		return CORSHandler(ctx)
-	}
-} */
